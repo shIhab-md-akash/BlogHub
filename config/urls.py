16fe754bdad2f5c10be74ffbf0a235a5710c276f
shihab-md-akash <shihabmdakash10@gmail.com>
2026-07-31 01:13:47 +0600

    Created base template and homepage UI

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Home Page
+    path("", include("core.urls")),
 ]
 
 if settings.DEBUG:
